97f9af225c7c7931dbcbcb6e3f1252e0c81f2276
Priyansh Pranjal <sceptile314159265@gmail.com>
2025-11-02 17:18:07 +0000

    REPL

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,14 @@ const std = @import("std");
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
+var stdin_buffer: [4096]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+const stdin = &stdin_reader.interface;
+
 pub fn main() !void {
-    try stdout.print("$ ", .{});
-
-    var reader_buffer: [1024]u8 = undefined;
-    var stdin_reader = std.fs.File.stdin().readerStreaming(&reader_buffer);
-    const stdin = &stdin_reader.interface;
-
-    var buffer: [1024]u8 = undefined;
-    var buffer_writer: std.Io.Writer = .fixed(&buffer);
-    const n = try stdin.streamDelimiter(&buffer_writer, '\n');
-
-    try stdout.print("{s}: command not found\n", .{buffer[0..n]});
+    while (true) {
+        try stdout.print("$ ", .{});
+        const command = try stdin.takeDelimiterExclusive('\n');
+        try stdout.print("{s}: command not found\n", .{command});
+    }
 }
