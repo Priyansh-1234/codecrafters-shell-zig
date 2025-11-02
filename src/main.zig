38a2cf3038e61bc952cff0b78208daac088667b9
Priyansh Pranjal <sceptile314159265@gmail.com>
2025-11-02 17:07:18 +0000

    Reading input

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,15 @@ var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
     try stdout.print("$ ", .{});
+
+    var reader_buffer: [1024]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().readerStreaming(&reader_buffer);
+    const stdin = &stdin_reader.interface;
+
+    var buffer: [1024]u8 = undefined;
+    var buffer_writer: std.Io.Writer = .fixed(&buffer);
+    const n = try stdin.streamDelimiter(&buffer_writer, '\n');
+
+    try stdout.print("{s}: command not found\n", .{buffer[0..n]});
 }
