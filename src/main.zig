9b2e03b8d820112ee090a220a8ab6377ca820d21
Priyansh Pranjal <sceptile314159265@gmail.com>
2025-11-02 18:10:26 +0000

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -7,24 +7,40 @@ var stdin_buffer: [4096]u8 = undefined;
 var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
 const stdin = &stdin_reader.interface;
 
+const commandFn = *const fn (args: []const u8) anyerror!void;
+
+fn echoFn(args: []const u8) !void {
+    try stdout.print("{s}\n", .{args});
+}
+
 pub fn main() !void {
+    var dba = std.heap.DebugAllocator(.{}){};
+    defer _ = dba.deinit();
+    const allocator = dba.allocator();
+
+    var command_functions = std.StringHashMap(commandFn).init(allocator);
+    defer command_functions.deinit();
+
+    try command_functions.put("echo", &echoFn);
+
     while (true) {
         try stdout.print("$ ", .{});
-        var arguments = try stdin.takeDelimiterInclusive('\n');
-        arguments = if (arguments[arguments.len - 1] == '\n') arguments[0 .. arguments.len - 1] else arguments;
-        var commands = std.mem.splitScalar(u8, arguments, ' ');
-        const command = commands.next();
-        if (command) |cmd| {
-            if (std.mem.eql(u8, "exit", cmd)) {
-                break;
-            } else if (std.mem.eql(u8, "echo", cmd)) {
-                while (commands.next()) |arg| {
-                    try stdout.print("{s} ", .{arg});
-                }
-                try stdout.print("\n", .{});
-            } else {
-                try stdout.print("{s}: command not found\n", .{cmd});
-            }
+
+        var command_input = try stdin.takeDelimiterInclusive('\n');
+        if (command_input[command_input.len - 1] == '\n') {
+            command_input = command_input[0 .. command_input.len - 1];
+        }
+
+        var arguments = std.mem.splitScalar(u8, command_input, ' ');
+        const command = arguments.first();
+        const args = arguments.rest();
+
+        if (std.mem.eql(u8, "exit", command)) break;
+
+        if (command_functions.get(command)) |func| {
+            try func(args);
+        } else {
+            try stdout.print("{s}: command not found\n", .{command});
         }
     }
 }
